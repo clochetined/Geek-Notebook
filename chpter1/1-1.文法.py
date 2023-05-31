e93d04c90b9ef806aa829e85c47c953e915d43c8
clochetined <naishutiantou@gmail.com>
2023-05-31 19:55:38 +0900

    基礎編

---
+++ chpter1/1-1.文法.py
@@ -0,0 +1,14 @@
+# input("weght[kg]")
+
+i = 0
+while i < 3:
+    # print(i)
+    if i == 1:
+        print('!!skip!!')
+        # break
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+
